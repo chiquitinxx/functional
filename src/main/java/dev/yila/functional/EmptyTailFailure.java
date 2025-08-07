f6954585c168c9886f6ed026e77c9499c719d79c
Jorge Franco <jorge.franco.leza@gmail.com>
2025-08-07 22:50:57 +0200

    Add license plugin and source headers

--- src/main/java/dev/yila/functional/EmptyTailFailure.java
+++
@@ -1,6 +0,0 @@
-package dev.yila.functional;
-
-import dev.yila.functional.failure.Failure;
-
-public class EmptyTailFailure implements Failure {
-}
--- src/main/java/dev/yila/functional/failure/EmptyTailFailure.java
+++ src/main/java/dev/yila/functional/failure/EmptyTailFailure.java
@@ -13,20 +13,5 @@
  */
 package dev.yila.functional.failure;
 
-public class DescriptionFailure implements Failure {
-
-    public static DescriptionFailure create(String description) {
-        return new DescriptionFailure(description);
-    }
-
-    private final String description;
-
-    DescriptionFailure(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return this.description;
-    }
+public class EmptyTailFailure implements Failure {
 }
